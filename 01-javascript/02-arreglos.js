ba658c6472ef9363e22b6d2fa6d60969426f2bd4
Erick Mayorga <erick.mayorga@epn.edu.ec>
2021-11-18 08:57:36 -0500

    update/ arreglos y for

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -1,1 +1,24 @@
-const arreglo = [6,7,8,9,10];
+//Arreglos
+let arreglo = [6,7,8,9,10];
+arreglo = 1;
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, "Erick", 'Mayorga', undefined, null, {}, [1,2]];
+arreglo = [6,7,8,9,10];
+
+//Bucles
+for (let numero of arreglo){
+    console.log('numero',numero);
+}
+
+for (let indice in arreglo){
+    arreglo[indice];
+    console.log('indice',indice);
+}
+
+let objetoPrueba = {a: '1', b: '2', c: '3'};
+for (let llave in objetoPrueba){
+    console.log('llave',llave);
+}
