56b0496ee77d6b003785129a6c74b08352b405f4
Erick Mayorga <erick.mayorga@epn.edu.ec>
2021-12-23 08:54:42 -0500

    update/ ngFor y ngStyle

--- 03-angular/ejemplo/src/app/rutas/ruta-login/ruta-login.component.ts
+++ 03-angular/ejemplo/src/app/rutas/ruta-login/ruta-login.component.ts
@@ -8,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class RutaLoginComponent implements OnInit {
 
   mostrarSegundoBanner = true
+  arregloUsuarios = [
+    {
+      id: 1,
+      nombre: 'Erick',
+      color: '#00BCFF',
+      link: 'https://www.supercines.com/cartelera/quito/6-de-diciembre/187',
+      linkImagen: 'http://assets.stickpng.com/images/584df3ad6a5ae41a83ddee08.png'
+    },
+    {
+      id: 2,
+      nombre: 'Andres',
+      color: '007AFF',
+      link: 'https://www.epn.edu.ec',
+      linkImagen: 'https://static.wikia.nocookie.net/amiibopedia/images/6/6d/Luigi.png/revision/latest?cb=20210109220240&path-prefix=es'
+    }
+  ]
 
   constructor() { }
 
