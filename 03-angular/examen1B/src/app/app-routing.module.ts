f21705852c0d80d5baf12a57c3864b1904a4aed3
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-01-13 12:37:35 -0500

    update/ final changes Exam 1B

--- 03-angular/examen1B/src/app/app-routing.module.ts
+++ 03-angular/examen1B/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeMicrosfotComponent} from "./rutas/home-microsfot/home-microsfot.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeMicrosfotComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {useHash: true})],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
