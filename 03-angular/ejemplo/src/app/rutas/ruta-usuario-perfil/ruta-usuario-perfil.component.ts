ba45ff4d83132008b18923737691389fd56e02c9
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-01-20 08:58:25 -0500

    update/ validators & errors

--- 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
+++ 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UserJphInterface} from "../../services/http/interfaces/user-jph.interface";
 import {UserJphService} from "../../services/http/user-jph.service";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+//import * as console from "console";
 
 @Component({
   selector: 'app-ruta-usuario-perfil',
@@ -24,7 +25,27 @@ export class RutaUsuarioPerfilComponent implements OnInit {
         value: 'ejemplo@ejemplo.com',
         disabled: false
       },
-        [])
+        [
+          Validators.required, //min,max,minLenght,email,patterns
+          Validators.minLength(3)
+        ],
+        [
+
+        ])
+    })
+    const cambio$ = this.formGroup.valueChanges
+    cambio$.subscribe({
+      next: (valor) => {
+        if(this.formGroup){
+          console.log(valor, this.formGroup)
+          if(this.formGroup?.valid){
+            console.log('YUPI')
+          }else{
+            console.log(':(')
+          }
+        }
+
+      }
     })
 
     const parametroRuta$ = this.activatedRoute.params;
